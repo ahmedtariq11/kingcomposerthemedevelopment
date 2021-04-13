9be4da5a3903ce17ee98f505f5a7c74d74f633d8
AHMED ALI TARIQ <70329164+ahmedtariq11@users.noreply.github.com>
2021-04-13 15:06:20 +0500

    Update tabs.php

--- wp-content/themes/xpent/woocommerce/single-product/tabs/tabs.php
+++ wp-content/themes/xpent/woocommerce/single-product/tabs/tabs.php
@@ -1,54 +1,123 @@
-<?php
-/**
- * Single Product tabs
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/single-product/tabs/tabs.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see 	https://docs.woothemes.com/document/template-structure/
- * @author  WooThemes
- * @package WooCommerce/Templates
- * @version 2.4.0
- */
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
-/**
- * Filter tabs and allow third parties to add their own.
- *
- * Each tab is an array containing title, callback and priority.
- * @see woocommerce_default_product_tabs()
- */
-$tabs = apply_filters( 'woocommerce_product_tabs', array() );
-
-if ( ! empty( $tabs ) ) : ?>
-
-	<div class="woocommerce-tabs wc-tabs-wrapper">
-		<ul class="tabs wc-tabs">
-			<?php foreach ( $tabs as $key => $tab ) : ?>
-				<li class="<?php echo esc_attr( $key ); ?>_tab">
-					<a href="#tab-<?php echo esc_attr( $key ); ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); ?></a>
-				</li>
-			<?php endforeach; ?>
-		</ul>
-		<?php foreach ( $tabs as $key => $tab ) : ?>
-			<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>">
-				<?php call_user_func( $tab['callback'], $key, $tab ); ?>
-			</div>
-		<?php endforeach; ?>
-	</div>
-	<div class="divider-d-dashed"></div>
-
-<?php endif; ?>
 
 
 
-
-
+<section class="ptb-60 ptb-xs-30">
+    <div class="container">
+      <div class="product-detail-tab">
+        <div class="row">
+          <div class="col-md-12">
+            <div id="tabs">
+              <ul class="nav nav-tabs">
+                <li><a class="tab-Description selected" title="Description">Description</a></li>
+                <li><a class="tab-Product-Tags" title="Product-Tags">Product-Tags</a></li>
+                <li><a class="tab-Reviews" title="Reviews">Reviews</a></li>
+              </ul>
+            </div>
+            <div id="items">
+              <div class="tab_content">
+                <ul>
+                  <li>
+                    <div class="items-Description selected gray-bg">
+                      <div class="Description"> 
+                           <?php wc_get_template('single-product/short-description.php'); ?>   
+                           <?php //woocommerce_output_product_data_tabs();?>     
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="items-Product-Tags gray-bg"><strong>Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC</strong><br />
+                      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur</div>
+                  </li>
+                  <li>
+                    <div class="items-Reviews gray-bg">
+                      <div class="comments-area">
+                        <h4>Comments<span>(2)</span></h4>
+                        <ul class="comment-list mt-30">
+                          <li>
+                            <div class="comment-user"> <img src="images/comment-user.jpg" alt="Xpent "> </div>
+                            <div class="comment-detail">
+                              <div class="user-name">John Doe</div>
+                              <div class="post-info">
+                                <ul>
+                                  <li>Fab 11, 2016</li>
+                                  <li><a href="#"><i class="fa fa-reply"></i>Reply</a></li>
+                                </ul>
+                              </div>
+                              <p>Consectetur adipiscing elit integer sit amet augue laoreet maximus nuncac.</p>
+                            </div>
+                            <ul class="comment-list child-comment">
+                              <li>
+                                <div class="comment-user"> <img src="images/comment-user.jpg" alt="Xpent "> </div>
+                                <div class="comment-detail">
+                                  <div class="user-name">John Doe</div>
+                                  <div class="post-info">
+                                    <ul>
+                                      <li>Fab 11, 2016</li>
+                                      <li><a href="#"><i class="fa fa-reply"></i>Reply</a></li>
+                                    </ul>
+                                  </div>
+                                  <p>Consectetur adipiscing elit integer sit amet augue laoreet maximus nuncac.</p>
+                                </div>
+                              </li>
+                              <li>
+                                <div class="comment-user"> <img src="images/comment-user.jpg" alt="Xpent "> </div>
+                                <div class="comment-detail">
+                                  <div class="user-name">John Doe</div>
+                                  <div class="post-info">
+                                    <ul>
+                                      <li>Fab 11, 2016</li>
+                                      <li><a href="#"><i class="fa fa-reply"></i>Reply</a></li>
+                                    </ul>
+                                  </div>
+                                  <p>Consectetur adipiscing elit integer sit amet augue laoreet maximus nuncac.</p>
+                                </div>
+                              </li>
+                            </ul>
+                          </li>
+                          <li>
+                            <div class="comment-user"> <img src="images/comment-user.jpg" alt="Xpent "> </div>
+                            <div class="comment-detail">
+                              <div class="user-name">John Doe</div>
+                              <div class="post-info">
+                                <ul>
+                                  <li>Fab 11, 2016</li>
+                                  <li><a href="#"><i class="fa fa-reply"></i>Reply</a></li>
+                                </ul>
+                              </div>
+                              <p>Consectetur adipiscing elit integer sit amet augue laoreet maximus nuncac.</p>
+                            </div>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="main-form mt-30">
+                        <h4>Leave a comments</h4>
+                        <div class="row mt-30">
+                          <form >
+                            <div class="col-sm-4 mb-30">
+                              <input type="text" placeholder="Name" required>
+                            </div>
+                            <div class="col-sm-4 mb-30">
+                              <input type="email" placeholder="Email" required>
+                            </div>
+                            <div class="col-sm-4 mb-30">
+                              <input type="text" placeholder="Website" required>
+                            </div>
+                            <div class="col-xs-12 mb-30">
+                              <textarea cols="30" rows="3" placeholder="Message" required></textarea>
+                            </div>
+                            <div class="col-xs-12 mb-30">
+                              <button class="btn-black" name="submit" type="submit">Submit</button>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
